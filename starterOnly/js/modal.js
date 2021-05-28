1c76d9695e50fed474242c782206c45632b2ad09
Mohamad Ghazal <mghazal1024@gmail.com>
2021-05-28 19:45:35 +0200

    style fixes and modal form opens and closes

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -9,15 +9,35 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBtn = [...document.querySelectorAll(".modal-btn")];
+const close = document.querySelector('.close');
 const formData = document.querySelectorAll(".formData");
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
-function launchModal() {
-  modalbg.style.display = "block";
+// function launchModal() {
+//   modalbg.style.display = "block";
+// }
+
+// function launchModal() {
+//   modalbg.classList.add('show');
+// }
+
+
+//Launch modal form
+const launchModal = () => {
+  modalbg.classList.add('show');
 }
 
+// Launch modal event
+for ( let btn of modalBtn ) {
+  btn.addEventListener('click', launchModal);
+}
 
+//Close Modal
+const closeModal = () => {
+  modalbg.classList.remove('show');
+}
+close.addEventListener('click', closeModal);
