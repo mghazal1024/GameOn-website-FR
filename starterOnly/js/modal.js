50fe4835da0c4fa28876dba85cde1a96e268ca26
Mohamad Ghazal <mghazal1024@gmail.com>
2021-05-29 22:28:33 +0200

    basic validation works

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -30,8 +30,6 @@ const formData = document.querySelectorAll(".formData");
 const launchModal = () => {
   modalbg.classList.add('show');
 }
-
-// Launch modal event
 for ( let btn of modalBtn ) {
   btn.addEventListener('click', launchModal);
 }
@@ -41,3 +39,145 @@ const closeModal = () => {
   modalbg.classList.remove('show');
 }
 close.addEventListener('click', closeModal);
+
+
+
+const form = document.querySelector('#form');
+const firstName = document.querySelector('#first');
+const lastName = document.querySelector('#last');
+const email = document.querySelector('#email');
+const date = document.querySelector('#birthdate');
+
+const showError = (input) => {
+  const parent = input.parentElement;
+  const error = parent.querySelector('.error-message');
+  input.classList.add('invalid');
+  input.classList.remove('valid');
+  error.style.display="block";
+}
+
+const showSuccess = (input) => {
+  const parent = input.parentElement;
+  const success = parent.querySelector('.error-message');
+  input.classList.remove('invalid');
+  input.classList.add('valid');
+  success.style.display="none";
+}
+
+const checkFirstName = () => {
+
+  let valid = false;
+  const min = 2;
+
+  if(firstName.value < min) {
+    showError(firstName);
+  } else {
+    showSuccess(firstName);
+    valid = true;
+  }
+  return valid;
+}
+
+const checkLastName = () => {
+
+  let valid = false;
+  const min = 2;
+
+  if(lastName.value < min) {
+    showError(lastName);
+  } else {
+    showSuccess(lastName);
+    valid = true
+  }
+  return valid;
+}
+
+const checkEmail = () => {
+  if(email.value == "") {
+    showError(email)
+  }
+}
+
+const checkDate = () => {
+  if(date.value == "") {
+    showError(date);
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let nameValid = checkFirstName();
+  let lastValid = checkLastName();
+  let emailValid = checkEmail();
+  let dateValid = checkDate();
+})
+
+form.addEventListener('input', (e) => {
+  switch (e.target.id) {
+    case 'first':
+      checkFirstName();
+      break;
+    case 'last':
+      checkLastName();
+      break;
+    case 'email':
+      checkEmail();
+      break;
+    case 'birthdate':
+      checkDate();
+      break;
+  }
+})
+
+
+
+
+// const formBtn = document.querySelector('.btn-submit');
+// const name = document.reserve.first.value;
+
+// const validate = () => {
+//   if(name == "") {
+//     alert ('do it');
+//     document.reserve.first.focus();
+//     return false;
+//   }
+//   return ( true );
+// }
+
+// const form = document.forms['reserve'];
+// const firstName = form['first'];
+// const lastName = form['last'];
+// const email = form['email'];
+// const date = form['birthdate'];
+
+// form.addEventListener('submit', (e) => {
+//   if(firstName.value == "") {
+//     e.preventDefault()
+//     firstName.querySelector('#first ~ p.error-message').style.display="block";
+//   } else {
+//     firstName.querySelector('#first ~ p.error-message').style.display="none";
+//   }
+
+//   if(lastName.value == "") {
+//     e.preventDefault()
+//     lastName.querySelector('#last ~ p.error-message').style.display="block";
+//   } else {
+//     lastName.querySelector('#last ~ p.error-message').style.display="none";
+//   }
+  
+//   if(email.value == "") {
+//     e.preventDefault()
+//     email.querySelector('#email ~ p.error-message').style.display="block";
+//   } else {
+//     email.querySelector('#email ~ p.error-message').style.display="none";
+//   }
+// })
+
+// const validate = () => {
+//   if(firstName.value == '') {
+//     alert('please add a name');
+//     firstName.focus();
+//     return false;
+//   }
+//   return true;
+// }
